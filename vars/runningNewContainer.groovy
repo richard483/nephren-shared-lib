685e8e513b8b8e784b323f6982555858ba6904cf
richard483 <richard.william483@gmail.com>
2024-12-28 02:20:49 +0700

    fix: made port definition to be optional

--- vars/runningNewContainer.groovy
+++ vars/runningNewContainer.groovy
@@ -1,4 +1,10 @@
 def call(String appPort, String containerName, String dockerImage) {
     echo 'Running new container...'
+
+    if (appPort == null || appPort.isEmpty()) {
+        sh "docker run -d --name ${containerName} --restart unless-stopped ${dockerImage}"
+        return
+    }
+
     sh "docker run -d -p ${appPort}:${appPort} --name ${containerName} --restart unless-stopped ${dockerImage}"
 }
