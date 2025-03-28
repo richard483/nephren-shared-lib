df0cec3dc028f607df4a1aebbc240423d2358416
richard483 <richard.william483@gmail.com>
2025-03-28 21:36:57 +0700

    config: update kube

--- vars/KubePipeline.groovy
+++ vars/KubePipeline.groovy
@@ -35,33 +35,34 @@ def call(body) {
                     // Create or update the ConfigMap
                     sh "kubectl create configmap ${CONTAINER_NAME}-config --from-literal=key=value --dry-run=client -o yaml | kubectl apply -f -"
 
-                    // Create a deployment with imagePullPolicy explicitly set to Never
+                    // Create a deployment with imagePullPolicy explicitly set to Never - with proper indentation
                     sh """
                         cat <<EOF > deployment.yaml
-            apiVersion: apps/v1
-            kind: Deployment
-            metadata:
-            name: ${CONTAINER_NAME}
-            labels:
-                app: ${CONTAINER_NAME}
-            spec:
-            replicas: 1
-            selector:
-                matchLabels:
-                app: ${CONTAINER_NAME}
-            template:
-                metadata:
-                labels:
-                    app: ${CONTAINER_NAME}
-                spec:
-                containers:
-                - name: ${CONTAINER_NAME}
-                    image: ${DOCKER_IMAGE}
-                    imagePullPolicy: Never
-                    ports:
-                    - containerPort: ${APP_PORT}
-            EOF
-                        kubectl apply -f deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${CONTAINER_NAME}
+  labels:
+    app: ${CONTAINER_NAME}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${CONTAINER_NAME}
+  template:
+    metadata:
+      labels:
+        app: ${CONTAINER_NAME}
+    spec:
+      containers:
+      - name: ${CONTAINER_NAME}
+        image: ${DOCKER_IMAGE}
+        imagePullPolicy: Never
+        ports:
+        - containerPort: ${APP_PORT}
+EOF
+                        cat deployment.yaml
+                        kubectl apply -f deployment.yaml || (echo "Deployment failed, see deployment.yaml above for details" && exit 1)
                         rm deployment.yaml
                         kubectl set env deployment/${CONTAINER_NAME} --from=configmap/${CONTAINER_NAME}-config
                     """
