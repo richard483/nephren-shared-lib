0cd43a40f671b003d7ccefe5277a9f5320aa2d99
richard483 <richard.william483@gmail.com>
2025-06-03 22:34:50 +0700

    patch: added DockerPipeline for better naming & keeping the global for old build

---
+++ vars/DockerPipeline.groovy
@@ -0,0 +1,51 @@
+def call(body) {
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    def DOCKER_IMAGE = pipelineParams.get('dockerImage')
+    def CONTAINER_NAME = pipelineParams.get('projectName')
+    def APP_PORT = pipelineParams.get('appPort')
+    def ENV_FILE = pipelineParams.get('envFile')
+    def NETWORK_NAME = pipelineParams.get('networkName')
+
+    pipeline {
+        agent any
+        stages {
+            stage('Checkout Code') {
+                steps {
+                    checkoutWithScm()
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    buildDockerImage(DOCKER_IMAGE, pipelineParams.get('buildArgs'))
+                }
+            }
+
+            stage('Deploy Application') {
+                steps {
+                    stoppingAndRemovingContainer(CONTAINER_NAME)
+                    createDockerNetwork(NETWORK_NAME)
+                    runningNewContainer(APP_PORT, CONTAINER_NAME, DOCKER_IMAGE, ENV_FILE, NETWORK_NAME)
+                }
+            }
+
+            stage('Removing Dangling Images') {
+                steps {
+                    removingDanglingImage()
+                }
+            }
+        }
+        post {
+            success {
+                echo 'Pipeline succeeded!'
+            }
+            failure {
+                echo 'Pipeline failed.'
+            }
+        }
+    }
+}
