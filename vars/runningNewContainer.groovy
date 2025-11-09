f6002ebeb132491cf37f1ce5c6638b454187f585
richard483 <richard.william483@gmail.com>
2025-11-09 23:49:58 +0700

    fix: removed extra comma on runningNewContainer.groovy

--- vars/runningNewContainer.groovy
+++ vars/runningNewContainer.groovy
@@ -1,6 +1,6 @@
 import com.nephren.DockerUtils
 
-def call(String appPort, String containerName, String dockerImage, String envFile, String networkName, , String volumeDriver) {
+def call(String appPort, String containerName, String dockerImage, String envFile, String networkName, String volumeDriver) {
     def utils = new DockerUtils(this)
     utils.runContainer(appPort, containerName, dockerImage, envFile, networkName, volumeDriver)
 }
