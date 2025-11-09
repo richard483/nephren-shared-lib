4e22fd6a0bcd55b584058dce8f9f38da46fd9383
richard483 <richard.william483@gmail.com>
2025-11-10 01:15:16 +0700

    fix

--- vars/checkoutWithCredential.groovy
+++ vars/checkoutWithCredential.groovy
@@ -2,5 +2,5 @@ import com.nephren.GitUtils
 
 def call(Integer checkoutTimeout, String repoUrl, String branch, String credentialsId) {
     def utils = new GitUtils(this)
-    utils.checkoutWithCredential(checkoutTimeout: checkoutTimeout, repoUrl: repoUrl, branch: branch, credentialsId: credentialsId)
+    utils.checkoutWithCredential(checkoutTimeout, repoUrl, branch, credentialsId)
 }
